287ee2f715e52e5846d203c1581e17f3a5ccbdcc
unknown <mdshahreerirfan@gmail.com>
2023-10-21 23:36:25 +0600

    first time java

---
+++ first_time.java
@@ -0,0 +1,9 @@
+public class first_time {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int i =10;
+        int j = 10;
+        System.out.println(i+j);
+    }
+}
+
