a71d2d985dc3f1755bdb51605a2db357725d30d0
unknown <mdshahreerirfan@gmail.com>
2023-10-24 15:12:01 +0600

    if else for loop done

--- first_time.java
+++ first_time.java
@@ -1,9 +1,19 @@
 public class first_time {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        int i =10;
+        int i =0;
         int j = 10;
         System.out.println(i+j);
+        for( i = 0; i<10;i++){
+            System.out.println(i);
+        }
+        int n =10;
+        if(n<11){
+            System.out.println("This condition is true");
+        }
+        else{
+            System.out.println("This condition is false");
+        }
         // This is java comment out system
     }
 }
