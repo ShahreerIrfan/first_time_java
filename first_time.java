286e51ae90fbe6ae4136714553848c3e7f5155b9
unknown <mdshahreerirfan@gmail.com>
2023-10-24 15:22:14 +0600

    prime composite

--- first_time.java
+++ first_time.java
@@ -14,7 +14,21 @@ public class first_time {
         else{
             System.out.println("This condition is false");
         }
-        // This is java comment out system
+        
+        int x = 10;
+        int flag  = 0;
+        for(i = 1 ;i<=10;i++){
+            if(x%i==0){
+                flag++;
+            }
+        }
+        System.out.println(flag);
+        if(flag<=2){
+            System.out.println("This is a prime numner");
+        }
+        else{
+            System.out.println("This is composite number");
+        }
     }
 }
 
